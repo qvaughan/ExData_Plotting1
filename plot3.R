476008bc72e468154ee1b6b31d63e19b6b815857
Michael Vaughan <jmichaelvaughan@gmail.com>
2019-11-17 16:13:11 -0500

    Updated after df to dt name change in load_data.R

--- plot3.R
+++ plot3.R
@@ -5,9 +5,9 @@ source('load_data.R')
 png('plot3.png', width=480, height=480)
 
 # Create the plot.
-plot(df$DateTime, df$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering', col='black')
-points(df$DateTime, df$Sub_metering_2, type='l', col='red')
-points(df$DateTime, df$Sub_metering_3, type='l', col='blue')
+plot(dt$DateTime, dt$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering', col='black')
+points(dt$DateTime, dt$Sub_metering_2, type='l', col='red')
+points(dt$DateTime, dt$Sub_metering_3, type='l', col='blue')
 legend("topright", col=c('black', 'red', 'blue'), legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty=1)
 
 # Write the PNG file.
--- plot4.R
+++ plot4.R
@@ -8,19 +8,19 @@ png('plot4.png', width=480, height=480)
 par(mfcol = c(2, 2))
 
 # Create the first plot.
-plot(df$DateTime, df$Global_active_power, type='l', xlab='', ylab='Global Active Power')
+plot(dt$DateTime, dt$Global_active_power, type='l', xlab='', ylab='Global Active Power')
 
 # Create the second plot.
-plot(df$DateTime, df$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering', col='black')
-points(df$DateTime, df$Sub_metering_2, type='l', col='red')
-points(df$DateTime, df$Sub_metering_3, type='l', col='blue')
+plot(dt$DateTime, dt$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering', col='black')
+points(dt$DateTime, dt$Sub_metering_2, type='l', col='red')
+points(dt$DateTime, dt$Sub_metering_3, type='l', col='blue')
 legend("topright", col=c('black', 'red', 'blue'), legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty=1, bty='n')
 
 # Create the third plot.
-plot(df$DateTime, df$Voltage, type='l', xlab='datetime', ylab='Voltage')
+plot(dt$DateTime, dt$Voltage, type='l', xlab='datetime', ylab='Voltage')
 
 # Create the fourth plot.
-plot(df$DateTime, df$Global_reactive_power, type='l', xlab='datetime', ylab='Global_reactive_power')
+plot(dt$DateTime, dt$Global_reactive_power, type='l', xlab='datetime', ylab='Global_reactive_power')
 
 # Write the PNG file.
 dev.off()
